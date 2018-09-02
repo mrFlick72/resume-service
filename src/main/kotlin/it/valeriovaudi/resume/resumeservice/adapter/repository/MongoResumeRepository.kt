e5390504dc1b632f6eadb778a820fc59ec4643d7
mrFlick72 <Back72Star>
2018-09-02 21:35:56 +0200

    crud api

--- src/main/kotlin/it/valeriovaudi/resume/resumeservice/adapter/repository/MongoResumeRepository.kt
+++ src/main/kotlin/it/valeriovaudi/resume/resumeservice/adapter/repository/MongoResumeRepository.kt
@@ -11,32 +11,45 @@ import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.data.mongodb.core.query.Update
 import reactor.core.publisher.Mono
+import reactor.core.publisher.toMono
 
-class MongoResumeRepository(private val mongoTemplate: ReactiveMongoTemplate) : ResumeRepository {
+class MongoResumeRepository(private val mongoTemplate: ReactiveMongoTemplate,
+                            private val personalDetailsRepository: MongoPersonalDetailsRepository) : ResumeRepository {
 
     companion object {
         fun collectionName() = "resume"
         fun findOneQuery(resumeId: String) = Query.query(Criteria.where("_id").`is`(resumeId))
+        fun findByUserNameAndLanguageQuery(userName: String, language: Language) =
+                Query.query(Criteria.where("userName").`is`(userName).and("language").`is`(language.name))
     }
-
 
     override fun findOne(resumeId: String): Publisher<Resume> =
             mongoTemplate.findOne(findOneQuery(resumeId), Document::class.java, collectionName())
                     .map { ResumeMapper.fromDocumentToDomain(it) }
+                    .flatMap { resume ->
+                        loadPersonalDetailsFor(resumeId).toMono()
+                                .map { Resume(id = resume.id, userName = resume.userName, language = resume.language, personalDetails = it) }
+                    }
 
-    override fun findOneByUserName(userName: String, language: Language): Publisher<Resume> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun findOneByUserName(userName: String, language: Language): Publisher<Resume> =
+            mongoTemplate.findOne(findByUserNameAndLanguageQuery(userName, language), Document::class.java, collectionName())
+                    .map { ResumeMapper.fromDocumentToDomain(it) }
+                    .flatMap { resume ->
+                        loadPersonalDetailsFor(resume.id).toMono()
+                                .map { Resume(id = resume.id, userName = resume.userName, language = resume.language, personalDetails = it) }
+                    }
+
 
     override fun save(resume: Resume): Publisher<Resume> =
             mongoTemplate.upsert(findOneQuery(resumeId = resume.id),
                     Update.fromDocument(ResumeMapper.fromDomainToDocument(resume)), collectionName())
                     .onErrorResume { println("Error at ${it}"); Mono.just(UpdateResult.unacknowledged()) }
+                    .flatMap { personalDetailsRepository.save(resumeId = resume.id, personalDetails = resume.personalDetails).toMono() }
                     .map { resume }
 
 
-    override fun delete(resumeId: String): Publisher<Unit> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun delete(resumeId: String): Publisher<Unit> = mongoTemplate.remove(findOneQuery(resumeId), collectionName()).flatMap { Mono.just(Unit) }
 
+
+    private fun loadPersonalDetailsFor(resumeId: String) = personalDetailsRepository.findOneWithoutPhoto(resumeId)
 }
--- src/test/kotlin/it/valeriovaudi/resume/resumeservice/adapter/repository/MongoResumeRepositoryTest.kt
+++ src/test/kotlin/it/valeriovaudi/resume/resumeservice/adapter/repository/MongoResumeRepositoryTest.kt
@@ -1,9 +1,12 @@
 package it.valeriovaudi.resume.resumeservice.adapter.repository
 
+import it.valeriovaudi.resume.resumeservice.TestCase
 import it.valeriovaudi.resume.resumeservice.domain.model.Language
+import it.valeriovaudi.resume.resumeservice.domain.model.PersonalDetails
 import it.valeriovaudi.resume.resumeservice.domain.model.Resume
 import org.assertj.core.api.Assertions
 import org.bson.Document
+import org.hamcrest.core.Is
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
@@ -13,6 +16,7 @@ import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.gridfs.GridFsTemplate
 import org.springframework.test.context.junit4.SpringRunner
 import reactor.core.publisher.toMono
 import java.time.Duration
@@ -25,18 +29,23 @@ class MongoResumeRepositoryTest {
     @Autowired
     lateinit var mongoTemplate: ReactiveMongoTemplate
 
+    @Autowired
+    lateinit var gridFsTemplate: GridFsTemplate
 
     lateinit var mongoResumeRepository: MongoResumeRepository
 
+    lateinit var mongoPersonalDetailsRepository: MongoPersonalDetailsRepository
+
     @Before
     fun setUp() {
-        mongoResumeRepository = MongoResumeRepository(mongoTemplate)
+        mongoPersonalDetailsRepository = MongoPersonalDetailsRepository(mongoTemplate, gridFsTemplate)
+        mongoResumeRepository = MongoResumeRepository(mongoTemplate, mongoPersonalDetailsRepository)
     }
 
     @Test
     fun `save a resume`() {
         val resumeId = UUID.randomUUID().toString()
-        val emptyResume = Resume.emptyResume(resumeId, "A_USER", Language.EN)
+        val emptyResume = Resume(resumeId, "A_USER", Language.EN, TestCase.personalDetailsWithPhoto())
 
         mongoResumeRepository.save(emptyResume).toMono().block(Duration.ofMinutes(1))
 
@@ -44,18 +53,60 @@ class MongoResumeRepositoryTest {
                 Document::class.java, "resume")
                 .block(Duration.ofMinutes(2))
 
+        println("details")
+        Assert.assertNotNull(mongoTemplate.findOne(Query.query(Criteria.where("resumeId").`is`(resumeId)),
+                Document::class.java, "personalDetails")
+                .block(Duration.ofMinutes(2)))
+
+        println("photo")
+
+        Assert.assertNotNull(gridFsTemplate.findOne(Query.query(Criteria.where("metadata.resumeId").`is`(resumeId))))
+        Assert.assertNotNull(gridFsTemplate.getResource(resumeId))
         Assert.assertNotNull(actualResumeDocument)
     }
 
     @Test
     fun `find a resume by id`() {
         val resumeId = UUID.randomUUID().toString()
-        val emptyResume = Resume.emptyResume(resumeId, "A_USER", Language.EN)
+        val emptyResume = Resume(resumeId, "A_USER", Language.EN, TestCase.personalDetailsWithPhoto())
 
         val actualResume = mongoResumeRepository.save(emptyResume).toMono()
                 .then(mongoResumeRepository.findOne(resumeId).toMono())
                 .block(Duration.ofMinutes(1))
 
         Assert.assertNotNull(actualResume)
+        Assert.assertThat(actualResume?.personalDetails?.firstName, Is.`is`("firstName"))
+    }
+
+    @Test
+    fun `find a resume by user name`() {
+        val resumeId = UUID.randomUUID().toString()
+        val emptyResume = Resume(resumeId, "A_USER", Language.EN, TestCase.personalDetailsWithPhoto())
+
+        val actualResume = mongoResumeRepository.save(emptyResume).toMono()
+                .then(mongoResumeRepository.findOneByUserName("A_USER", Language.EN).toMono())
+                .block(Duration.ofMinutes(1))
+
+        Assert.assertNotNull(actualResume)
+        Assert.assertThat(actualResume?.personalDetails?.firstName, Is.`is`("firstName"))
+    }
+
+    @Test
+    fun `delete a resume`() {
+        val resumeId = UUID.randomUUID().toString()
+        val emptyResume = Resume(resumeId, "A_USER", Language.EN, TestCase.personalDetailsWithPhoto())
+
+        mongoResumeRepository.save(emptyResume).toMono()
+                .then(mongoResumeRepository.delete(resumeId).toMono())
+                .block(Duration.ofMinutes(1))
+
+        println("details")
+        Assert.assertNotNull(mongoTemplate.findOne(Query.query(Criteria.where("resumeId").`is`(resumeId)),
+                Document::class.java, "personalDetails")
+                .block(Duration.ofMinutes(2)))
+
+        println("photo")
+        Assert.assertNotNull(gridFsTemplate.findOne(Query.query(Criteria.where("metadata.resumeId").`is`(resumeId))))
+        Assert.assertNotNull(gridFsTemplate.getResource(resumeId))
     }
 }
