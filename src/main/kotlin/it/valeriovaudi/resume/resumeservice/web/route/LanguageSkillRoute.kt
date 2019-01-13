210b8b57e225a6d64455d1277928cf8b8e2aeede
mrFlick72 <valerio.vaudi@gmail.com>
2019-01-13 12:07:22 +0100

    read all language skills and refactoring

--- src/main/kotlin/it/valeriovaudi/resume/resumeservice/web/route/LanguageSkillRoute.kt
+++ src/main/kotlin/it/valeriovaudi/resume/resumeservice/web/route/LanguageSkillRoute.kt
@@ -5,10 +5,10 @@ import it.valeriovaudi.resume.resumeservice.domain.repository.LanguageSkillsRepo
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.router
 import reactor.core.publisher.toMono
-import java.net.URI
 
 
 @Configuration
@@ -17,30 +17,19 @@ class LanguageSkillRoute {
     @Bean
     fun languageSkillRoutes(@Value("\${baseServer:http://localhost:8080}") baseServer: String,
                             languageSkillsRepository: LanguageSkillsRepository) = router {
-        POST("/resume/{resumeId}/language-skill")
+
+        GET("/resume/{resumeId}/language-skills")
+        {
+            languageSkillsRepository.findOne(it.pathVariable("resumeId")).toMono()
+                    .flatMap { ServerResponse.ok().body(BodyInserters.fromObject(it)) }
+        }
+
+        PUT("/resume/{resumeId}/language-skills")
         {
             val resumeId = it.pathVariable("resumeId")
             it.bodyToMono(LanguageSkills::class.java)
                     .flatMap { languageSkillsRepository.save(resumeId, it).toMono() }
-                    .flatMap { ServerResponse.created(URI("$baseServer/resume/$resumeId/language-skill")).build() }
-        }
-
-        GET("/resume/{resumeId}/language-skill")
-        {
-            //            workExperienceRepository.findAll(it.pathVariable("resumeId")).toFlux()
-//                    .collectList()
-//                    .flatMap { ServerResponse.ok().body(BodyInserters.fromObject(it)) }
-            TODO()
-        }
-
-        PUT("/resume/{resumeId}/language-skill")
-        {
-            //            val resumeId = it.pathVariable("resumeId")
-//            val educationId = it.pathVariable("educationId")
-//            it.bodyToMono(WorkExperienceRepresentation::class.java)
-//                    .flatMap { workExperienceRepository.save(resumeId, WorkExperienceRepresentation.fromRepresentationToDomain(it, educationId)).toMono() }
-//                    .flatMap { ServerResponse.noContent().build() }
-            TODO()
+                    .flatMap { ServerResponse.noContent().build() }
         }
     }
 
--- src/test/kotlin/it/valeriovaudi/resume/resumeservice/web/route/LanguageSkillRouteTest.kt
+++ src/test/kotlin/it/valeriovaudi/resume/resumeservice/web/route/LanguageSkillRouteTest.kt
@@ -1,5 +1,6 @@
 package it.valeriovaudi.resume.resumeservice.web.route
 
+import it.valeriovaudi.resume.resumeservice.TestCase
 import it.valeriovaudi.resume.resumeservice.adapter.repository.MongoLanguageSkillsRepository
 import it.valeriovaudi.resume.resumeservice.domain.model.LanguageCapabilityLevel.*
 import it.valeriovaudi.resume.resumeservice.domain.model.LanguageSkill
@@ -7,8 +8,6 @@ import it.valeriovaudi.resume.resumeservice.domain.model.LanguageSkills
 import it.valeriovaudi.resume.resumeservice.domain.model.Speaking
 import it.valeriovaudi.resume.resumeservice.domain.model.Understanding
 import it.valeriovaudi.todolist.TestContextInitializer
-import org.hamcrest.core.Is
-import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -18,9 +17,9 @@ import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.reactive.server.WebTestClient
-import org.springframework.test.web.reactive.server.returnResult
 import org.springframework.web.reactive.function.BodyInserters
-import java.net.URI
+import reactor.core.publisher.toMono
+import java.time.Duration
 import java.util.*
 
 @ContextConfiguration(initializers = [TestContextInitializer::class])
@@ -34,40 +33,39 @@ class LanguageSkillRouteTest {
     @Autowired
     private lateinit var languageSkillsRepository: MongoLanguageSkillsRepository
 
+    val languageSkills = LanguageSkills("A_NATIVE_LANGUAGE",
+            listOf(LanguageSkill("A_LANGUAGE",
+                    Understanding(B1, C1),
+                    Speaking(A1, C1), C1)))
 
     @Test
     @WithMockUser(username = "user")
     fun `save language skill`() {
         val resumeId = UUID.randomUUID().toString()
-        val languageSkills = LanguageSkills("A_NATIVE_LANGUAGE",
-                listOf(LanguageSkill("A_LANGUAGE",
-                        Understanding(B1, C1),
-                        Speaking(A1, C1), C1)))
-        val location =  webClient.post()
-                .uri("/resume/${resumeId}/language-skill")
+
+        webClient.put()
+                .uri("/resume/${resumeId}/language-skills")
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(BodyInserters.fromObject(languageSkills))
                 .exchange()
-                .expectStatus().isCreated
-                .returnResult<Any>().responseHeaders.location
-
-        Assert.assertNotNull(location)
-        Assert.assertThat(URI("http://localhost:8080/resume/$resumeId/language-skill") , Is.`is`(location))
+                .expectStatus().isNoContent
 
     }
 
     @Test
     @WithMockUser(username = "user")
     fun `read language skill`() {
-//        personalDetailsRepository.save("RESUME_ID", TestCase.personalDetails()).toMono().block();
-//
-//        val expectedJson = TestCase.readFileAsString("personal-details.json")
-//        webClient.get()
-//                .uri("/resume/RESUME_ID/personal-details")
-//                .accept(MediaType.APPLICATION_JSON)
-//                .exchange()
-//                .expectStatus().isOk
-//                .expectBody().json(expectedJson)
+        val resumeId = UUID.randomUUID().toString()
+
+        languageSkillsRepository.save("$resumeId", languageSkills).toMono().block(Duration.ofMinutes(1));
+
+        val expectedJson = TestCase.readFileAsString("language-skills.json")
+        webClient.get()
+                .uri("/resume/$resumeId/language-skills")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk
+                .expectBody().json(expectedJson)
     }
 
     @Test
